adb7e061714d2db6ebf4f983b4f36f70b0f9dbf6
Oscott <os281411@gmail.com>
2026-07-20 12:38:16 +0100

    Initial Commit

---
+++ Quotidian/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quotidian
+//
+//  Created by Oliver Scott on 20/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quotidian/QuotidianApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotidianApp.swift
+//  Quotidian
+//
+//  Created by Oliver Scott on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct QuotidianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
